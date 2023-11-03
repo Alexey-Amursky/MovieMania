66283cfbed86a7a5b95f8e351cf50515c8fc736f
Alexey Amursky <Alexey@Amur-sky.ru>
2023-11-03 14:42:26 +0300

    Добавил класс для работы с файлами FileHandler

---
+++ Filmography/FileHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Filmography
+{
+    public class FileHandler
+    {
+        public void SaveMovies(string filePath, List<Movie> movies)
+        {
+            string json = JsonSerializer.Serialize(movies);
+            File.WriteAllText(filePath, json);
+        }
+
+        public List<Movie> LoadMovies(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<List<Movie>>(json);
+            }
+            else
+            {
+                return new List<Movie>();
+            }
+        }
+    }
+}
