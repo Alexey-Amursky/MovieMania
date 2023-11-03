4110a949e57f2e5658602212e207e8c95aaf7122
Alexey Amursky <Alexey@Amur-sky.ru>
2023-11-03 14:40:46 +0300

    Добавил класс MovieLibrary

---
+++ Filmography/MovieLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmography
+{
+    public class MovieLibrary
+    {
+        private List<Movie> movies = new List<Movie>();
+
+        public void AddMovie(Movie movie)
+        {
+            movies.Add(movie);
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return movies;
+        }
+    }
+}
