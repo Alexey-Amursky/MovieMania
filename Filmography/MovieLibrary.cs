c60d27968c30968ab7cc7b7e514a39b8c79eb636
Alexey Amursky <Alexey@Amur-sky.ru>
2023-11-06 19:11:37 +0300

    так же обновил return

--- Filmography/MovieLibrary.cs
+++ Filmography/MovieLibrary.cs
@@ -29,30 +29,12 @@ namespace Filmography
                 movies.RemoveAt(index);
             }
         }
-        public List<Movie> SearchMoviesByTitle(string title)
-        {
-            return movies.Where(movie => movie.Title.ToLower().Contains(title)).ToList();
-        }
-        public List<Movie> SearchMoviesByDirector(string director)
-        {
-            return movies.Where(movie => movie.Director.ToLower().Contains(director)).ToList();
-        }
-        public List<Movie> SearchMoviesByYear(int year)
-        {
-            return movies.Where(movie => movie.Year == year).ToList();
-        }
-        public List<Movie> SearchMoviesByGenre(Genre genre)
-        {
-            return movies.Where(movie => movie.Genres.Contains(genre)).ToList();
-        }
-        public List<Movie> GetAllMovies()
-        {
-            return movies;
-        }
-        public int GetMovieCount()
-        {
-            return movies.Count;
-        }
+        public List<Movie> SearchMoviesByTitle(string title) => movies.Where(movie => movie.Title.ToLower().Contains(title)).ToList();
+        public List<Movie> SearchMoviesByDirector(string director) => movies.Where(movie => movie.Director.ToLower().Contains(director)).ToList();
+        public List<Movie> SearchMoviesByYear(int year) => movies.Where(movie => movie.Year == year).ToList();
+        public List<Movie> SearchMoviesByGenre(Genre genre) => movies.Where(movie => movie.Genres.Contains(genre)).ToList();
+        public List<Movie> GetAllMovies() => movies;
+        public int GetMovieCount() => movies.Count;
 
     }
 }
