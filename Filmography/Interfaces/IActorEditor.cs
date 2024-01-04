d3c2f776ece95205047104b3958817d339f397e6
Alexey Amursky <Alexey@Amur-sky.ru>
2024-01-04 21:53:24 +0300

    Добавил интерфейс и методы для работы с актёрами

---
+++ Filmography/Interfaces/IActorEditor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmography.Interfaces
+{
+    public interface IActorEditor
+    {
+        void AddActor(Actor actor);
+        void EditActor(int movieIndex, int actorIndex, Actor updatedActor);
+        void DeleteActor(int movieIndex, int actorIndex);
+    }
+}
