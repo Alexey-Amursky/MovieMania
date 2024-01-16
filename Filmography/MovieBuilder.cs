e6efcf01d6e6257af84e896571fd41101b148ccc
Alexey Amursky <Alexey@Amur-sky.ru>
2024-01-16 17:46:28 +0300

    Добавил null значимые типы

--- Filmography/MovieBuilder.cs
+++ Filmography/MovieBuilder.cs
@@ -20,55 +20,55 @@ namespace Filmography
             return movie;
         }
 
-        public MovieBuilder SetTitle(string title)
+        public MovieBuilder SetTitle(string? title)
         {
             movie.Title = title;
             return this;
         }
 
-        public MovieBuilder SetDirector(string director)
+        public MovieBuilder SetDirector(string? director)
         {
             movie.Director = director;
             return this;
         }
 
-        public MovieBuilder SetYear(int year)
+        public MovieBuilder SetYear(int? year)
         {
             movie.Year = year;
             return this;
         }
 
-        public MovieBuilder SetGenres(List<Genre> genres)
+        public MovieBuilder SetGenres(List<Genre>? genres)
         {
             movie.Genres = genres;
             return this;
         }
 
-        public MovieBuilder SetRating(decimal rating)
+        public MovieBuilder SetRating(decimal? rating)
         {
             movie.Rating = rating;
             return this;
         }
 
-        public MovieBuilder SetActors(List<Actor> actors)
+        public MovieBuilder SetActors(List<Actor>? actors)
         {
             movie.SetActors(actors);
             return this;
         }
 
-        public MovieBuilder SetAgeRating(AgeRating ageRating)
+        public MovieBuilder SetAgeRating(AgeRating? ageRating)
         {
             movie.AgeRating = ageRating;
             return this;
         }
 
-        public MovieBuilder SetDescription(string description)
+        public MovieBuilder SetDescription(string? description)
         {
             movie.Description = description;
             return this;
         }
 
-        public MovieBuilder SetPath(string path)
+        public MovieBuilder SetPath(string? path)
         {
             movie.Path = path;
             return this;
